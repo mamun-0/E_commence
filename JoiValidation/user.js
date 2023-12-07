152d60f175ddedba8add5c8c2758c43c1a8b9d13
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-12-08 03:20:21 +0600

    validateUser object properties by JOI

---
+++ JoiValidation/user.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+const validateUser = (user) => {
+  return Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    email: Joi.string().email().max(255).required(),
+    password: Joi.string().min(5).nax(255).required(),
+  }).validate(user);
+};
+
+module.exports = {
+  validateUser,
+};
