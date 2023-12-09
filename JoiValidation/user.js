89d634fa882e78abdfaf2458ced628a87d9ba6ff
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-12-09 11:44:00 +0600

    fix nax to max function name

--- JoiValidation/user.js
+++ JoiValidation/user.js
@@ -3,7 +3,7 @@ const validateUser = (user) => {
   return Joi.object({
     name: Joi.string().min(3).max(100).required(),
     email: Joi.string().email().max(255).required(),
-    password: Joi.string().min(5).nax(255).required(),
+    password: Joi.string().min(5).max(255).required(),
   }).validate(user);
 };
 
