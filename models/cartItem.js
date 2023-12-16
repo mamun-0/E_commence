e9d31846d1e5764c265a854afc6f677ea701f444
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-12-16 13:07:58 +0600

    CartItem model

---
+++ models/cartItem.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const cartItemSchema = new Schema(
+  {
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    price: Number,
+    count: {
+      type: Number,
+      default: 1,
+      min: 1,
+      max: 5,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports.CartItemSchema = cartItemSchema;
+module.exports.CartItem = model("CartItem", cartItemSchema);
